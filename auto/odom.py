a3d078f131bd25db1a63b8a8ecf00db3bec597ac
stv <stevanuschang946@gmail.com>
2026-09-13 21:15:26 +0700

    reset

--- auto/odom.py
+++ auto/odom.py
@@ -16,8 +16,6 @@ class Odometri():
         self.y = 0
         self.theta = 0
 
-        self.frequency = 0
-
         self.prev_dist_x_right = 0
         self.prev_dist_x_left = 0
         self.prev_dist_y = 0
