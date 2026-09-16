04f626cd67c5cb946a8772d2050f6fbd1e7b53eb
stv <stevanuschang946@gmail.com>
2026-09-16 10:43:28 +0700

    adding deadwheel

--- auto/modbus.py
+++ auto/modbus.py
@@ -14,23 +14,14 @@ class Modbus(Node):
 
         self.timer = self.create_timer(self.frequency, self.timerCallback)
 
-    def convertToFloat(self, low, high):
-        combined = (high << 16) | low
-        return struct.unpack('<f', struct.pack('<I', combined))[0]
-
     def timerCallback(self):
-        result_x_right = self.client_.read_input_registers(address=0, count=2, slave=1)
-        result_x_left = self.client_read_input_registers(address=0, count=2, slave=2)
-        result_y = self.client_.read_input_registers(addrees=0, count=2, slave=3)
-
-        distance_x_right = self.convertToFloat(result_x_right.registers[0], result_x_right.registers[1])
-        distance_x_left = self.convertToFloat(result_x_left.registers[0], result_x_left.registers[1])
-        distance_y = self.convertToFloat(result_y.registers[0], result_y.registers[1])
+        result_x_right = self.client_.read_input_registers(address=0, count=1, slave=1)
+        result_x_left = self.client_read_input_registers(address=0, count=1, slave=2)
+        result_y = self.client_.read_input_registers(addrees=0, count=1, slave=3)
 
         msg = UInt16MultiArray()
-        msg.data = [distance_x_right, distance_x_left, distance_y]
+        msg.data = [result_x_right, result_x_left, result_y]
         self.pub_.publish(msg)
-
 
 def main():
     rclpy.init()
